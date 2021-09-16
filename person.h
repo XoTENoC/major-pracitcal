102bc5d71d4913ba634f01294eae91a3964cc3a0
sophiedavidson <87119888+sophiedavidson@users.noreply.github.com>
2021-09-16 15:45:07 +0930

    added person.h

---
+++ person.h
@@ -0,0 +1,35 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+
+#include<string>
+using namespace std; 
+
+class person{
+    public:
+    person();
+    std::string personName;
+    std::string getPersonName();
+    void setPersonName(std::string personName);
+    int soundCapability;
+    int getSoundCapability();
+    void setSoundCapability(int capability);
+    int lightingCapability;
+    int getLightingCapability();
+    void setLightingCapability(int capability);
+    int computerGraphicsCapability;
+    int getComputerGraphicsCapability();
+    void setComputerGraphicsCapability(int capability);
+    int age;
+    int getAge();
+    void setAge(int age);
+    int contactNumber;
+    void setContactNumber();
+    int getContactNumber(int contactNumber);
+
+
+
+};
+
+
+#endif
