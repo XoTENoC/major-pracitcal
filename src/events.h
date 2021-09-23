bd12b47a3311c35e6c66ef928292d15d83a546fa
XoTENoC <Changipro.nathaniel@gmail.com>
2021-09-24 00:02:54 +0930

    adding functions to the events class

--- src/events.h
+++ src/events.h
@@ -3,17 +3,35 @@
 
 #include <string>
 #include "person.h"
-#include "adult.h"
-#include "child.h"
 
 using namespace std;
 
+// This class is defined for each week of the rooster, this houses all the
+// people within the week. It holds the event name, what week of the roster it
+// is and how many people are on the roster for that week
+
 class events{
     public:
+        // Variable within the week.
         int numberOfStaffNeeded;
         person* rosterOfPeople;
         string eventName;
-        int weelNumber;
+        int weekNumber;
+
+        //functions of the class
+        events();
+        events(int numberOfStaffNeeded, person* rosterOfPeople, string eventName, int weekNumber);
+        
+        int getNumberOfStaffNeeded();
+        void setNumberOfStaffNeeded(int numberOfStaff);
+
+        int getWeekNumber();
+        void setWeekNumber(int weekNumber);
+
+        string getEventName();
+        void setEventName();
+
+        ~events();
 };
 
 #endif //  EVENTS_H
