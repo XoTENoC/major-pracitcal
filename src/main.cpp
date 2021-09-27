b0f69bf5a71ba938b39a0121afe4ebcd04bec82f
Austen Lindsay <aalindsay@me.com>
2021-09-27 20:47:31 +0930

    adding main menu
    adding switch statement for a main menu
    
    Co-Authored-By: Nathaniel Chang <41539419+XoTENoC@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,57 @@
 
 using namespace std;
 
+void addPerson(person * arr, person * arrTemp, int * ammount){
+
+
+}
+
 int main(){
     bool inProgram = true;
-    
+    bool mainMenu = true;
+
+    // keeping a track of all the people created
+    int numberOfPeople = 0;
+    person * arrayOfPeople;
+    person * tempArrayOfPerople;
+
+    int choice = 0;
+
 
     while(inProgram){
-        // program.
+        // MAIN MENU
+        mainMenu = true;
+        while(mainMenu){
+            cout << "1 - Add a new person" << endl;
+            cout << "2 - View all the people" << endl;
+            cout << "3 - Create a roster" << endl;
+            cout << "4 - View roster" << endl;
+            cout << "5 - Exit" << endl;
+
+            cin >> choice;
+
+            switch(choice){
+                // adding a person the array of people
+                case 1:
+                    mainMenu = false;
+                    addPerson(arrayOfPeople, tempArrayOfPerople, &numberOfPeople);
+                    break;
+                case 2:
+                    mainMenu = false;
+                    break;
+                case 3:
+                    mainMenu = false;
+                    break;
+                case 4:
+                    mainMenu = false;
+                    break;
+                case 5:
+                    mainMenu = false;
+                    inProgram = false;
+                    break;
+            }
+        }
+
     }
 
 }
