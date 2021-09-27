4645eba0ca59b3d822e6df9fb39c2aeb6247fbc5
Austen Lindsay <aalindsay@me.com>
2021-09-27 20:27:39 +0930

    Created main.cpp
    Created the structure for the main.cpp file

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+#include "roster.h"
+#include "person.h"
+#include "events.h"
+
+using namespace std;
+
+int main(){
+    bool inProgram = true;
+    
+
+    while(inProgram){
+        // program.
+    }
+
+}
