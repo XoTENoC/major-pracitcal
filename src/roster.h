33eb37769d29f61a3bd9929c561f63370cfea471
XoTENoC <Changipro.nathaniel@gmail.com>
2021-09-27 20:14:26 +0930

    defining the function in the roster class

--- src/roster.h
+++ src/roster.h
@@ -10,6 +10,9 @@ using namespace std;
 class roster{
     public:
 
+    roster();
+    roster(int eventsAmmount);
+
     // if needed in the future more positions can be added with a function.
     string positions[4] = {"Team Leader", "Sound", "Lighting", "Computer Graphics"};
     int numberOfNeeededPositions = 4;
@@ -19,6 +22,7 @@ class roster{
 
     int getNumberOfEvents();
     int setNumberOfEvents(int eventsAmmount);
+    
 };
 
 #endif
