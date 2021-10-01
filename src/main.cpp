2a1d98b3ca66e6b2b50d74ceb04fdf7d1eb691e9
Nathaniel Chang <Changipro.nathaniel@gmail.com>
2021-10-01 23:38:36 +0930

    Moving and organizing folders

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
-#include "roster.h"
-#include "person.h"
-#include "adult.h"
-#include "child.h"
-#include "events.h"
+#include "classes/roster.h"
+#include "classes/person.h"
+#include "classes/adult.h"
+#include "classes/child.h"
+#include "classes/events.h"
 
 using namespace std;
 
@@ -28,167 +28,9 @@ void addPerson(person ** arr, int * ammount){
         switch (option){
             case 1:
                 isTrue = false;
-                // creating a new array person
-
-                // checking to make sure that there are already people in the
-                // array
-                if(*ammount>0){
-
-                    // starting the temp array to store people
-                    tempArrayOfPerople = new person*[size];
-                    cout << "created temp array" << endl;
-
-                    // moving all there perople into the array
-                    for (int i = 0; i < *ammount; i++){
-                        arr[i] = tempArrayOfPerople[i];
-                        cout << i << endl;
-                    }
-
-                    cout << "Made it" << endl;
-
-                    // Should be Deleting the array but causing a segmentation fault
-                    // delete[] arr;
-
-                    cout << "Made it" << endl;
-
-                    // id of people incease
-                    *ammount += 1;
-
-                    cout << "Made it" << endl;
-
-                    // making the array one person bigger
-                    arr = new person*[*ammount];
-
-                    cout << "Made it" << endl;
-
-                    // Asking for the name
-                    cout << "name for the person: ";
-                    cin >> name;
-
-                    // Asking for the age
-                    cout << "Age of the person: ";
-                    cin >> age;
-
-                    arr[*ammount-1] = new adult(name, age);
-
-                    cout << arr[*ammount-1]->getPersonName() << endl;
-
-                    // moving people into the new array
-                    for (int i = 0; i < *ammount-1; i++){
-                        tempArrayOfPerople[i] = arr[i];
-                    }
-                    
-                    // Deleting the temp array
-                    delete[] tempArrayOfPerople;
-                }
-                
-                // if there are no people in the array make the array 1.
-                else if( *ammount == 0 ){
-                    arr = new person*[1];
-                    *ammount += 1;
-
-                    // Asking for the name
-                    cout << "name for the person: ";
-                    cin >> name;
-
-                    // Asking for the age
-                    cout << "Age of the person: ";
-                    cin >> age;
-
-                    arr[*ammount-1] = new adult(name, age);
-
-                    cout << arr[0]->getPersonName() << endl;
-                }
-
-                // // assigning all the competencies of the person
-                // cout << "ability of sound: ";
-                // cin >> competency;
-
-                // arr[*ammount]->setCompetency(0, competency);
-
-                // cout << "ability of lighting: ";
-                // cin >> competency;
-
-                // arr[*ammount]->setCompetency(1, competency);
-
-                // cout << "ability of computer graphics: ";
-                // cin >> competency;
-
-                // arr[*ammount]->setCompetency(2, competency);
-
                 break;
             case 2:
                 isTrue = false;
-
-                // // creating a new array person
-
-                // // checking to make sure that there are already people in the
-                // // array
-                // if(*ammount>0){
-
-                //     // starting the temp array to store people
-                //     tempArrayOfPerople = new person[*ammount];
-
-                //     // moving all there perople into the array
-                //     for (int i = 0; i < *ammount; i++){
-                //         arr[i] = tempArrayOfPerople[i];
-                //     }
-
-                //     // making room for the next array
-                //     delete[] arr;
-
-                //     // id of people incease
-                //     *ammount += 1;
-
-                //     // making the array one person bigger
-                //     arr = new person[*ammount]; 
-
-                //     // moving people into the new array
-                //     for (int i = 0; i < *ammount-1; i++){
-                //         tempArrayOfPerople[i] = arr[i];
-                //     }
-                    
-                //     // Deleting the temp array
-                //     delete[] tempArrayOfPerople;
-                // }
-
-                // // if there are no people in the array make the array 1.
-                // else if( *ammount == 0 ){
-                //     delete[] arr;
-                //     arr = new person[1];
-                //     *ammount += 1;
-                // }
-
-                // // Asking for the name
-                // cout << "name for the person: ";
-                // cin >> name;
-
-                // // Asking for the age
-                // cout << "Age of the person: ";
-                // cin >> age;
-
-                // // assinging the name and the age
-                // newChild.setPersonName(name);
-                // newChild.setAge(age);
-
-                // // assigning the person to the array in the last spot
-                // arr[*ammount] = newChild;
-
-                // // assigning all the competencies of the person
-                // cout << "ability of sound: ";
-                // cin >> competency;
-
-                // arr[*ammount].setCompetency(0, competency);
-
-                // cout << "ability of lighting: ";
-                // cin >> competency;
-
-                // arr[*ammount].setCompetency(1, competency);
-
-                // cout << "ability of computer graphics: ";
-                // cin >> competency;
-
-                // arr[*ammount].setCompetency(2, competency);
 
                 break;
             case 3:
@@ -209,15 +51,12 @@ void viewAllPeople(person ** arr, int * ammount){
 }
 
 int main(){
+
+    // Starting the program
     bool inProgram = true;
     bool mainMenu = true;
 
-    // keeping a track of all the people created
-    int numberOfPeople = 0;
-    person ** arrayOfPeople;
-
     int choice = 0;
-
 
     while(inProgram){
         // MAIN MENU
@@ -235,11 +74,10 @@ int main(){
                 // adding a person the array of people
                 case 1:
                     mainMenu = false;
-                    addPerson(arrayOfPeople, &numberOfPeople);
                     break;
                 case 2:
                     mainMenu = false;
-                    viewAllPeople(arrayOfPeople, &numberOfPeople);
+                    // viewAllPeople(arrayOfPeople, &numberOfPeople);
                     break;
                 case 3:
                     mainMenu = false;
--- src/testingArr.cpp
+++ src/testingArr.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <memory>
 #include "person.h"
 #include "adult.h"
 
