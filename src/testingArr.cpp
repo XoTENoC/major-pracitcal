f709ea1049ac7127f0f32a62dc5b421eecaf3974
Nathaniel Chang <Changipro.nathaniel@gmail.com>
2021-10-01 23:23:52 +0930

    Update testingArr.cpp
    Removing dynamic alocaiton and using vectors
    and its working

--- src/testingArr.cpp
+++ src/testingArr.cpp
@@ -7,86 +7,12 @@
 
 using namespace std;
 
-// // adding a new person to an array
-// void addingNewPerson(person ** arr, int * ammount, string name, int age){
+void addPerson(vector<person*> &apple, int * ammount){
 
-//     // when there is nothing in the array at the start of the program
-//     if (*ammount == 0){
-
-//         // creating an array of size one
-//         arr = new person*[1];
-
-//         // adding an adult with name and age
-//         arr[0] = new adult(name, age);
-        
-//         *ammount++;
-
-//         // printing the name of the person added to the array
-//         cout << arr[0]->getPersonName() << endl; // the program works here
-//     }
-//     else {
-
-//         // init temp array
-//         person ** temp;
-
-//         // Creating the temp array to be the right size.
-//         temp = new person*[*ammount];
-
-//         // copying everything into the array
-//         for (int i = 0; i < *ammount; i++){
-//             arr[i] = temp[i];
-//         }
-
-//         // deleting the array
-//         delete[] arr;
-
-//         *ammount++;
-
-//         // creating the array at the new size
-//         arr = new person*[*ammount];
-
-//         // copying everything into the new array
-//         for (int i = 0; i < *ammount-1; i++){
-//             temp[i] = arr[i];
-//         }
-
-//         // adding the new person to the end of the array
-//         arr[*ammount-1] = new adult(name, age);
-
-//         // freeing the temp array
-//         delete[] temp;
-
-//     }
-
-// }
-
-int main(){
-
-    // // init the array
-    // person ** arrayOfPeople;
-    // arrayOfPeople = nullptr;
-
-    // int ammount = 0;
-
-    // // adding a new person to the array
-    // addingNewPerson(arrayOfPeople, &ammount, "nathaniel", 18);
-
-    // // when I try to print the name of the person here, i get a segmentation
-    // // fault.
-    // cout << arrayOfPeople[0]->getPersonName() << endl;
-
-    //Trying out vectors
-
-    int ammount = 0;
     string name;
     int age;
 
-    vector<person*> people;
-
-    cout << "How many people: ";
-    cin >> ammount;
-
-    for (int i = 0; i < ammount; i++){
+    for (int i = 0; i < *ammount; i++){
 
         // asking for the name of the person
         cout << "what is the name of the person: ";
@@ -96,10 +22,27 @@ int main(){
         cout << "What is the age of the person: ";
         cin >> age;
 
-        people.push_back(new adult(name, age));
+        apple.push_back(new adult(name, age));
     }
 
-    for (int i = 0; i < (int)people.size(); i++){
+}
+
+int main(){
+
+    //Trying out vectors
+
+    int ammount = 0;
+
+    vector<person*> people;
+
+    cout << "How many people: ";
+    cin >> ammount;
+
+    addPerson(people, &ammount);
+
+    cout << (int)people.size() << endl;
+
+    for (int i = 0; i < ammount; i++){
         cout << people[i]->getPersonName() << endl;
     }
 
