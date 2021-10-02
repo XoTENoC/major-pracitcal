726d6f62089a7626fb2aebc2cf012f7489176a3a
Nathaniel Chang <Changipro.nathaniel@gmail.com>
2021-10-03 10:10:02 +1030

    updating addingPerson.cpp
    Changed to that instead of adding Adults or Children, Now if the ages is less than 18 it will add a child and if the age is 18 and older and adult is created

--- src/functions/addingPerson.cpp
+++ src/functions/addingPerson.cpp
@@ -11,7 +11,7 @@
 
 // this function adds adults to the list of people, and also allows to add
 // multiple people at once to allow for fast batch additions of people.
-void addingAdult(vector<person*> &vectorPeople){
+void addingPerson(vector<person*> &vectorPeople){
 
     cout << endl;
 
@@ -36,78 +36,56 @@ void addingAdult(vector<person*> &vectorPeople){
         cout << "What is the age of the person: ";
         cin >> age;
 
-        // adding the person to the array
-        vectorPeople.push_back(new adult(name, age));
+        if (age >= 18)
+        {
+            cout << "adult Created" << endl;
+            // adding the person to the array
+            vectorPeople.push_back(new adult(name, age));
 
-        // setting the competency
-        cout << "ability at Sound: ";
-        cin >> competency;
+            // setting the competency
+            cout << "ability at Sound: ";
+            cin >> competency;
 
-        vectorPeople.back()->setCompetency(0, competency);
+            vectorPeople.back()->setCompetency(0, competency);
 
-        cout << "ability at Lighing: ";
-        cin >> competency;
+            cout << "ability at Lighing: ";
+            cin >> competency;
 
-        vectorPeople.back()->setCompetency(1, competency);
+            vectorPeople.back()->setCompetency(1, competency);
 
-        cout << "ability at Computer Graphics: ";
-        cin >> competency;
+            cout << "ability at Computer Graphics: ";
+            cin >> competency;
 
-        vectorPeople.back()->setCompetency(2, competency);
+            vectorPeople.back()->setCompetency(2, competency);
+        }
+        else
+        {
+            cout << "child Created" << endl;
+            // adding the person to the array
+            vectorPeople.push_back(new child(name, age));
+
+            // setting the competency
+            cout << "ability at Sound: ";
+            cin >> competency;
+
+            vectorPeople.back()->setCompetency(0, competency);
+
+            cout << "ability at Lighing: ";
+            cin >> competency;
+
+            vectorPeople.back()->setCompetency(1, competency);
+
+            cout << "ability at Computer Graphics: ";
+            cin >> competency;
+
+            vectorPeople.back()->setCompetency(2, competency);
+        }
     }
 
 }
 
 
-// this function adds children to the list of people, and also allows to add
-// multiple people at once to allow for fast batch additions of people.
-void addingChild(vector<person*> &vectorPeople){
-
-    cout << endl;
-
-    int ammountOfPeople = 0;
-    string name;
-    int age, competency;
-
-    // asking how many people the user would like to add
-    cout << "how many people would you like to add: ";
-    cin >> ammountOfPeople;
-
-    for (int i = 0; i < ammountOfPeople; i++){
-
-        cout << endl;
-        cout << "Person " << i+1 << endl;
-
-        // asking for the name of the person
-        cout << "what is the name of the person: ";
-        cin >> name;
-
-        //asking for the age of the person
-        cout << "What is the age of the person: ";
-        cin >> age;
-
-        // adding the person to the array
-        vectorPeople.push_back(new child(name, age));
-
-        // setting the competency
-        cout << "ability at Sound: ";
-        cin >> competency;
-
-        vectorPeople.back()->setCompetency(0, competency);
-
-        cout << "ability at Lighing: ";
-        cin >> competency;
-
-        vectorPeople.back()->setCompetency(1, competency);
-
-        cout << "ability at Computer Graphics: ";
-        cin >> competency;
-
-        vectorPeople.back()->setCompetency(2, competency);
-    }
-
-}
-
+// This function will print the list of people to the console
 void listAllpeople(vector<person*> &vectorPeople){
     cout << endl;
     cout << "+------------------------------------------------------+" << endl;
@@ -116,7 +94,6 @@ void listAllpeople(vector<person*> &vectorPeople){
     cout << endl;
     
     // listing all the people in the list
-
     int allThePeople = (int)vectorPeople.size();
 
     for(int i = 0; i < allThePeople; i++){
@@ -152,13 +129,12 @@ void addPerson(vector<person*> &vectorPeople, int * ammount){
         switch (choice)
         {
         case 1:
-            addingAdult(vectorPeople);
+            addingPerson(vectorPeople);
             *ammount = (int)vectorPeople.size();
             break;
 
         case 2:
-            addingChild(vectorPeople);
-            *ammount = (int)vectorPeople.size();
+            addingPeople = false;
             break;
 
         case 3:
