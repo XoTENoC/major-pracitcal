445998a26784f2167cdc6516176de7c3fc061ffb
sophiedavidson <87119888+sophiedavidson@users.noreply.github.com>
2021-10-07 11:04:02 +1030

    Revert "Revert "added code to function folder, removed from main""
    This reverts commit 2dfeb0c5142c6c287e6a2a5ab52b2ad9f92f5122.

---
+++ src/functions/addingRoster.h
@@ -0,0 +1,18 @@
+#ifndef ADDINGROSTER_H
+#define ADDINGROSTER_H
+
+#include<vector>
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "../classes/roster.h"
+#include "../classes/person.h"
+#include "../classes/adult.h"
+#include "../classes/child.h"
+#include "../classes/events.h"
+
+void createRoster(vector<person*> people, int numberOfStaffTotal);
+
+
+#endif 
