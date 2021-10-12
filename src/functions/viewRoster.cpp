b10bf8c7f562633c0933680c233ac42a8afa8bc0
sophiedavidson <87119888+sophiedavidson@users.noreply.github.com>
2021-10-12 21:56:36 +1030

    viewRoster pretty printer complete

--- src/functions/viewRoster.cpp
+++ src/functions/viewRoster.cpp
@@ -10,24 +10,41 @@
 using namespace std;
 
 void displayRoster(vector<events*> allEvents){
+    
 
-    // Gets the number of the events
+    cout << "+------------------------------------------------------+" << endl;
+    cout << "|                    View Roster                       |" << endl;
+    cout << "+------------------------------------------------------+" << endl;   
+    cout<<endl;
+     // Gets the number of the events
     int numberOfEvents = (int)allEvents.size();
+    
+    //defines the meaning of array positions
+    string positionsArray3[7] = {"Team Lead       ","Sound           ","Lighting        ","CG              ", "Trainee Sound   ","Trainee Lighting",  "Trainee CG      "};
+
 
     // for all the events
     for (int i = 0; i < numberOfEvents; i++){
 
         // getting the number of the people in the roster
         int numberOfPeople = allEvents[i]->getNumberOfStaffNeeded();
-
         // printing out all the people
+
+    cout << "+------------------------------------------------------+" << endl;
+    cout << "|                    Week "<<i+1<<"                            |" << endl;
+ 
+            
+
+
         for (int j = 0; j < numberOfPeople; j++){
-            cout << allEvents[i]->rosterOfPeople[j]->getPersonName() << " ";
+            cout << "+------------------------------------------------------+" << endl;
+            cout << "| "<<positionsArray3[j]<<"  | "<< allEvents[i]->rosterOfPeople[j]->getPersonName()<<"                           |" << endl;
+           // cout << allEvents[i]->rosterOfPeople[j]->getPersonName() << " ";
         }
 
         // printing the new line
-        cout << endl;
+        cout << "+------------------------------------------------------+" << endl;
 
+    
     }
-
 }
