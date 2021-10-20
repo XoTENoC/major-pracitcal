ff042e1e2b10e46a0efbc8c9a984061ab7256e19
Austen Lindsay <aalindsay@me.com>
2021-10-20 13:20:40 +1030

    Revert adult.h
    reverted adult.h back 1 commit

--- src/classes/adult.h
+++ src/classes/adult.h
@@ -1,11 +1,4 @@
-/* 
-
-adult.h
-
-Adult class inherits from parent class "person" to add behaviours and
-states which are unique to adults only.
-
-*/
+// Adult class inherits from parent class "person" to add behaviours and states which are unique to adults only.
 
 #include "person.h"
 #ifndef ADULT_H
@@ -18,25 +11,19 @@ class adult: public person{
 
     public:
 
-        // Constructor derived of abstract person class.
-        adult(std::string aName, int aAge): person(){
-            personName = aName;
-            age = aAge;
-        }
+    // Constructor derived of abstract person class.
+    adult(std::string aName, int aAge): person(){
+        personName = aName;
+        age = aAge;
+    }
 
-        // takes in a string of any length and assigns it to the variable
-        // workingWithChildrenNumber
-        void setWorkingWithChildrenNumber(string aWorkingWithChildrenNumber);
-        // returns the variable workingWithChildrenNumber
-        string getWorkingWithChildrenNumber();
+    void setWorkingWithChildrenNumber(string aWorkingWithChildrenNumber);
+    string getWorkingWithChildrenNumber();
 
-        // takes in a string of any length for the email and string of anylength
-        // for the phone number
-        void addContactInformation(std::string emailAddress, std::string phoneNumber);
-        void removeContactInformation();
+    void addContactInformation(std::string emailAddress, std::string phoneNumber);
+    void removeContactInformation();
 
-        // returning the contant information to the user.
-        virtual void getContactInformation();
+    virtual void getContactInformation();
 
     private:
         string workingWithChildrenNumber;
